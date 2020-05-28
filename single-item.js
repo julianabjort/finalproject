32edf8cbf553bb52fc1f9e85be5fc0b6efbd5705
Laufey Hjaltested <laufeyhjaltested@Laufeys-MacBook-Pro.local>
2020-05-28 20:25:22 +0200

    singleitemJS

---
+++ single-item.js
@@ -0,0 +1,27 @@
+//SINGLE ITEM//
+window.addEventListener('DOMContentLoaded', getData);
+
+const datalink = "http://andreimihutoni.com/wp_tate/wp-json/wp/v2/item?_embed";
+
+function getData() {
+    const urlParams = new URLSearchParams(window.location.search);
+    console.log("URLSearchParams " + window.location);
+    const the_item_id = urlParams.get("item_id");
+    console.log(the_item_id);
+
+    fetch("http://andreimihutoni.com/wp_tate/wp-json/wp/v2/item/" + the_item_id + "?_embed")
+        .then(res => res.json())
+        .then(showItems)
+
+}
+function showItems(item) {
+    console.log(item)
+    const template = document.querySelector("template").content;
+
+    const copy = template.cloneNode(true);
+
+    copy.querySelector(".singleImage").src = item.image.guid;
+
+    //Putting the copy of the template into main
+    document.querySelector("main").appendChild(copy);
+}

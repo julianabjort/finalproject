7b1009637589ab16f90a0a9b1cc7d9f47b10bda7
Laufey Hjaltested <laufeyhjaltested@Laufeys-MacBook-Pro.local>
2020-05-29 15:47:51 +0200

    instagramfeed

---
+++ script.js
@@ -0,0 +1,31 @@
+//###############   Instagram short feed    #########
+
+
+
+const url = 'https://graph.instagram.com/me/media?fields=id,caption,media_url,permalink&access_token=IGQVJVdFFSb1F6d0JOWnI3OVhmbks0VC1uSndkblBtRi1rTUNWcUJGQjNPeXhMRDNEQk9KNmM1TlpZAcU5VX2Q1a0xTa3J1MkpGMXdWUGdKQnZAzRkExdk1VSm9vNXB6Qld4cTlZAWGIzZAWNwY1ZA2WDc4SgZDZD';
+
+fetch(url)
+    .then(res => res.json())
+    .then(handleData)
+
+function handleData(feed) {
+//    console.log(feed)
+
+    for (let i = 0; i <= 3; i++) {
+        showFeedFrontPage(feed.data[i])
+        //        console.log(x)
+    }
+}
+
+
+
+
+
+function showFeedFrontPage(feed) {
+    const templateFrontPage = document.querySelector("#templateWrapperFrontPage").content;
+    const clone = templateFrontPage.cloneNode(true);
+
+    clone.querySelector(".postImg").src = feed.media_url;
+//    console.log(feed.media_url)
+    document.querySelector(".instaFeed").appendChild(clone);
+}

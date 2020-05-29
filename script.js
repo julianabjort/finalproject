9052e49460be91d9a4853c2df6076202c8b4af21
Laufey Hjaltested <laufeyhjaltested@Laufeys-MacBook-Pro.local>
2020-05-29 15:54:41 +0200

    sticky nav

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
+//STICKY NAV BAR//
+window.onscroll = function() {myFunction()};
+
+var nav = document.getElementById("nav");
+var sticky = nav.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    nav.classList.add("sticky")
+  } else {
+    nav.classList.remove("sticky");
+  }
+}
+
 //###############   Instagram short feed    #########
-
-
-
 const url = 'https://graph.instagram.com/me/media?fields=id,caption,media_url,permalink&access_token=IGQVJVdFFSb1F6d0JOWnI3OVhmbks0VC1uSndkblBtRi1rTUNWcUJGQjNPeXhMRDNEQk9KNmM1TlpZAcU5VX2Q1a0xTa3J1MkpGMXdWUGdKQnZAzRkExdk1VSm9vNXB6Qld4cTlZAWGIzZAWNwY1ZA2WDc4SgZDZD';
 
 fetch(url)

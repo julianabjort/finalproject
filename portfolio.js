fdaac5b7f2a9cf0f63edbe7d2ac5f669ab385c5d
Laufey Hjaltested <laufeyhjaltested@Laufeys-MacBook-Pro.local>
2020-06-01 19:43:22 +0200

    insta hover function

--- portfolio.js
+++ portfolio.js
@@ -2,9 +2,6 @@
 
 const url = 'https://graph.instagram.com/me/media?fields=id,caption,media_url,permalink&access_token=IGQVJVdFFSb1F6d0JOWnI3OVhmbks0VC1uSndkblBtRi1rTUNWcUJGQjNPeXhMRDNEQk9KNmM1TlpZAcU5VX2Q1a0xTa3J1MkpGMXdWUGdKQnZAzRkExdk1VSm9vNXB6Qld4cTlZAWGIzZAWNwY1ZA2WDc4SgZDZD';
 
-
-
-
 fetch(url)
     .then(res => res.json())
     .then(handleInstagramData)
@@ -22,8 +19,22 @@ function showFeedPortofolio(feed) {
     const templatePortofolio = document.querySelector("#templateWrapperPortofolio").content;
     const clone = templatePortofolio.cloneNode(true);
 
-    clone.querySelector(".postImgPortofolio").src = feed.media_url;
-    document.querySelector(".instaFeedPortofolio").appendChild(clone);
+     const caption = feed.caption;
+        console.log(caption)
+    const triggerCaption = caption.search("#thesehappydoodles")
+//    console.log(triggerCaption)
+
+    if (triggerCaption >= 0) {
+        clone.querySelector(".postImgPortofolio").src = feed.media_url;
+        clone.querySelector(".captionOnHover").textContent = feed.caption;
+        clone.querySelector(".userNameOnHover").textContent = feed.username;
+
+        clone.querySelector(".OnHoverWrapper").addEventListener('click', function () {
+            window.open("https://www.instagram.com/these.happy.doodles/", '_blank');
+        })
+
+        document.querySelector(".instaFeedPortofolio").appendChild(clone);
+}
 }
 
 // When the user scrolls the page, execute myFunction

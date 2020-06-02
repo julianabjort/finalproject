7568d2ac825e351b8e8401c6d8424ea9481b110b
Andrei-Mihutoni <andrei.mihutoni@gmail.com>
2020-06-02 08:17:23 +0200

    HomePage - intsgaram feed captions

--- script.js
+++ script.js
@@ -10,19 +10,36 @@ fetch(url)
 function handleData(feed) {
 //    console.log(feed)
 
-    for (let i = 0; i <= 3; i++) {
+    for (let i = 0; i <= 4; i++) {
         showFeedFrontPage(feed.data[i])
         //        console.log(x)
     }
 }
 
+
+
 function showFeedFrontPage(feed) {
     const templateFrontPage = document.querySelector("#templateWrapperFrontPage").content;
     const clone = templateFrontPage.cloneNode(true);
 
-    clone.querySelector(".postImg").src = feed.media_url;
-//    console.log(feed.media_url)
-    document.querySelector(".instaFeed").appendChild(clone);
+    const caption = feed.caption;
+        console.log(caption)
+    const triggerCaption = caption.search("#thesehappydoodles")
+//    console.log(triggerCaption)
+
+
+
+    if (triggerCaption >= 0) {
+        clone.querySelector(".postImgFrontPage").src = feed.media_url;
+        clone.querySelector(".captionOnHoverFrontPage").textContent = feed.caption;
+        clone.querySelector(".userNameOnHoverFrontPage").textContent = feed.username;
+
+        clone.querySelector(".OnHoverWrapperFrontPage").addEventListener('click', function () {
+            window.open("https://www.instagram.com/these.happy.doodles/", '_blank');
+        })
+
+        document.querySelector(".instaFeedFrontPage").appendChild(clone);
+    }
 }
 
 

b77959c4f02dc4f58dd2badc59ea2b14aa38a36e
Andrei-Mihutoni <andrei.mihutoni@gmail.com>
2020-06-04 12:01:46 +0200

    cart in the nav 1

--- portfolio.js
+++ portfolio.js
@@ -1,61 +1,54 @@
 //  ###   Instagram feed basic display  ###
 
 const url = 'https://graph.instagram.com/me/media?fields=id,caption,media_url,permalink&access_token=IGQVJVdFFSb1F6d0JOWnI3OVhmbks0VC1uSndkblBtRi1rTUNWcUJGQjNPeXhMRDNEQk9KNmM1TlpZAcU5VX2Q1a0xTa3J1MkpGMXdWUGdKQnZAzRkExdk1VSm9vNXB6Qld4cTlZAWGIzZAWNwY1ZA2WDc4SgZDZD';
-
 fetch(url)
     .then(res => res.json())
     .then(handleInstagramData)
-
 function handleInstagramData(feed) {
-        console.log(feed.data)
-
+    console.log(feed.data)
     for (let i = 0; i <= 50; i++) {
         showFeedPortofolio(feed.data[i])
-                console.log(i)
+        console.log(i)
     }
 }
-
 function showFeedPortofolio(feed) {
     const templatePortofolio = document.querySelector("#templateWrapperPortofolio").content;
     const clone = templatePortofolio.cloneNode(true);
-
-     const caption = feed.caption;
-        console.log(caption)
+    const caption = feed.caption;
     const triggerCaption = caption.search("#thesehappydoodles")
-//    console.log(triggerCaption)
-
     if (triggerCaption >= 0) {
         clone.querySelector(".postImgPortofolio").src = feed.media_url;
         clone.querySelector(".captionOnHover").textContent = feed.caption;
         clone.querySelector(".userNameOnHover").textContent = feed.username;
-
         clone.querySelector(".OnHoverWrapper").addEventListener('click', function () {
             window.open("https://www.instagram.com/these.happy.doodles/", '_blank');
         })
-
         document.querySelector(".instaFeedPortofolio").appendChild(clone);
-}
+    }
 }
 
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+window.onscroll = function () {
+    myFunction()
+};
 
 var overflow = document.getElementById("myTopnav")
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-      overflow.classList.remove("topnavOverflow")
-  }
+    overflow.classList.remove("topnavOverflow")
+}
 var btn = document.getElementById("click")
-btn.onclick = function() {myFunction1()};
+btn.onclick = function () {
+    myFunction1()
+};
 
 
 function myFunction1() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "navbar" || x.className === "navbar topnavOverflow") {
-    x.className += " responsive";
-  } else {
-    x.className = "navbar";
-  }
+    var x = document.getElementById("myTopnav");
+    if (x.className === "navbar" || x.className === "navbar topnavOverflow") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
 }
-
